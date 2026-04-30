0e56e6e307e4346fb85c7032944759a27ee20d66
manasacse777-sudo <manasa.cse777@gmail.com>
2026-04-30 00:18:46 -0400

    Change default node count to 1 in AKS variables

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -16,7 +16,7 @@ variable "aks_name" {
     variable "node_count" {
       description = "The number of nodes in the AKS cluster."
       type        = number
-      default     = 3
+      default     = 1
     }
     variable "node_vm_size" {
       description = "The size of the virtual machines for the AKS cluster nodes."
